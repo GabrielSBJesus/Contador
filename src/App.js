5ef6fc2f6d49757b81cbfa2ecefaa625686fa0a8
Gabriel <gabrielsantosbomjesus@gmail.com>
2022-11-25 11:43:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+      counter: 0,
+    };
+    timeOutUpdate = null;
+
+
+    handleTimeOut = () => {
+      const {counter} = this.state;
+
+      this.timeOutUpdate = setTimeout(() => {
+        this.setState({counter: counter + 1})
+      }, 1000)
+    }
+
+    componentDidMount() {
+      this.handleTimeOut()
+    }
+
+    componentDidUpdate() {
+      this.handleTimeOut()
+    }
+
+    componentWillUnmount() {
+      clearTimeout(this.timeOutUpdate)
+    }
+
+
+  render() {
+    const { counter } = this.state;
+
+    return (
+      <div className="App">
+        <p className="number">{counter}</p>  
+      </div>
+    );
+  }
 }
+
+  // function App() {
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <p>
+  //           {eu}
+  //         </p>
+  //         <a
+  //           className="App-link"
+  //           href="https://reactjs.org"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Learn React
+  //         </a>
+  //       </header>
+  //     </div>
+  //   );
+  // }
 
 export default App;
