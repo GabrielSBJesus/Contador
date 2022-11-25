ae4a5dbc27e08d68e9a56e19be5e2eab35b9aa3e
Gabriel <gabrielsantosbomjesus@gmail.com>
2022-11-25 11:44:37 -0300

    update

--- src/App.js
+++ src/App.js
@@ -41,25 +41,4 @@ class App extends Component {
   }
 }
 
-  // function App() {
-  //   return (
-  //     <div className="App">
-  //       <header className="App-header">
-  //         <img src={logo} className="App-logo" alt="logo" />
-  //         <p>
-  //           {eu}
-  //         </p>
-  //         <a
-  //           className="App-link"
-  //           href="https://reactjs.org"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //         >
-  //           Learn React
-  //         </a>
-  //       </header>
-  //     </div>
-  //   );
-  // }
-
 export default App;
